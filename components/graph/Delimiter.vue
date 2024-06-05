3f1ee1f98728382a35f1eb20d104aa8aff009360
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-06-05 19:32:36 +0500

    Update Delimiter.vue

--- components/graph/Delimiter.vue
+++ components/graph/Delimiter.vue
@@ -1,18 +1,15 @@
 <template>
-  <v-line :config="config"></v-line>
+  <v-line
+    :config="{
+      stroke: '#f4d56f',
+      strokeWidth: 1,
+      points: [0, currentY, stage.width, currentY],
+    }"
+  ></v-line>
 </template>
 
 <script>
 export default {
   props: ['stage', 'currentY'],
-  computed: {
-    config() {
-      return {
-        stroke: '#ffe7a3',
-        strokeWidth: 1,
-        points: [0, this.currentY, this.stage.width, this.currentY],
-      }
-    },
-  },
 }
 </script>

d0a132d8de78b0461ed34822233a75496100afd5
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-06-23 16:39:14 +0500

    Update timer.client.js

--- plugins/timer.client.js
+++ plugins/timer.client.js
@@ -1,14 +1,8 @@
 export default defineNuxtPlugin((nuxtApp) => {
   let zero = performance.now()
-  let counter = 0
   const manage = () => {
-    if (performance.now() - zero > 90) {
+    if (performance.now() - zero > 100) {
       nuxtApp.$bus.emit('nanoSec')
-      counter++
-      if (counter === 10) {
-        nuxtApp.$bus.emit('sec')
-        counter = 0
-      }
       zero = performance.now()
     }
     window.requestAnimationFrame(manage)

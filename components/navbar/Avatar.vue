e9a1c95424af873e4263d1116be8f048535907e2
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-06-25 13:23:08 +0500

    process

--- components/navbar/Avatar.vue
+++ components/navbar/Avatar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="avatar" class="avatar btn btn-circle btn-ghost h-10 min-h-10 w-10" role="button" tabindex="0">
-    <div class="h-10 w-10 cursor-pointer overflow-hidden rounded-full bg-black">
+  <div v-if="avatar" class="avatar h-10 min-h-10 w-10" role="button" tabindex="0">
+    <div class="h-10 w-10 rounded-full bg-black">
       <img :src="avatar" class="rounded-full" alt="" />
     </div>
   </div>

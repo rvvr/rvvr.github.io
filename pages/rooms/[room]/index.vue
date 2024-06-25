3115cb21d3f962c020f3a79d7e8bfb28973550ce
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-06-25 16:58:18 +0500

    Room rating

--- pages/rooms/[room]/index.vue
+++ pages/rooms/[room]/index.vue
@@ -2,12 +2,12 @@
   <NavbarView>
     <template #center>
       <div class="join">
-        <NavbarBalance />
-
         <NuxtLink :to="`/rooms/${$route.params.room}/rating`">
+          <NavbarBalance />
+
           <button class="btn btn-secondary join-item btn-lg -mt-8 pt-4">
             <IconsTrophy class="h-4 w-4 opacity-80" />
-            <span class="font-oswald text-xl font-bold leading-none">9/10</span>
+            <span class="font-oswald text-xl font-bold leading-none">2/6</span>
           </button>
         </NuxtLink>
       </div>
--- pages/rooms/[room]/rating.vue
+++ pages/rooms/[room]/rating.vue
@@ -8,4 +8,33 @@
       <NuxtLink :to="`/rooms/${$route.params.room}`"><NavbarClose /></NuxtLink>
     </template>
   </NavbarView>
+
+  <div class="p-4">
+    <div class="overflow-x-auto">
+      <table class="table">
+        <thead>
+          <tr>
+            <th class="w-9"></th>
+            <th clas>Player</th>
+            <th class="w-28 text-center">Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in 6" :class="{ 'bg-base-300 font-bold text-neutral-content': i == 2 }">
+            <th>{{ i }}</th>
+            <td>
+              <div class="flex items-center">
+                <img :src="`https://robohash.org/${i}.png?set=set3`" class="mr-2 h-8 w-8" />
+
+                John Doe
+              </div>
+            </td>
+            <td :class="[i > 4 ? 'font-bold uppercase text-red-500' : 'text-lime-500']" class="text-center">
+              {{ i > 4 ? 'out' : 1150 }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>

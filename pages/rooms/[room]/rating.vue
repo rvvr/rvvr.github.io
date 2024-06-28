c51436f5dd316edbe76b10b52cb30e987eedd8ed
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-06-28 15:27:15 +0500

    Update rating.vue

--- pages/rooms/[room]/rating.vue
+++ pages/rooms/[room]/rating.vue
@@ -9,32 +9,70 @@
     </template>
   </NavbarView>
 
-  <div class="p-4">
-    <div class="overflow-x-auto">
-      <table class="table">
-        <thead>
-          <tr>
-            <th class="w-9"></th>
-            <th clas>Player</th>
-            <th class="w-28 text-center">Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="i in 6" :class="{ 'bg-base-300 font-bold text-neutral-content': i == 2 }">
-            <th>{{ i }}</th>
-            <td>
-              <div class="flex items-center">
-                <img :src="`https://robohash.org/${i}.png?set=set3`" class="mr-2 h-8 w-8" />
+  <Loader :data="data">
+    <div class="p-4">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <thead>
+            <tr>
+              <th class="w-9"></th>
+              <th>Player</th>
+              <th class="w-28 text-center">Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(row, i) in sorted"
+              :key="i"
+              :class="{ 'bg-base-300 font-bold text-neutral-content': i == 2 }"
+            >
+              <th>{{ row.position }}</th>
+              <td>
+                <div class="flex items-center">
+                  <img
+                    :src="row.avatar_url ? row.avatar_url : `https://robohash.org/${i}.png?set=set3`"
+                    class="mr-2 h-8 w-8"
+                  />
 
-                John Doe
-              </div>
-            </td>
-            <td :class="[i > 4 ? ' uppercase text-red-500' : 'text-lime-500']" class="text-center font-bold">
-              {{ i > 4 ? 'out' : 1150 }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                  {{ row.username }}
+                </div>
+              </td>
+              <td
+                :class="[row.balance ? 'text-lime-500' : 'uppercase text-red-500']"
+                class="text-center font-bold"
+              >
+                {{ row.balance ? row.balance : 'out' }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>
+  </Loader>
 </template>
+
+<script>
+import { mapActions } from 'pinia'
+
+export default {
+  data() {
+    return {
+      data: null,
+    }
+  },
+
+  methods: {
+    ...mapActions(useRoomStore, ['getRoomRating']),
+  },
+
+  computed: {
+    sorted() {
+      return this.data.sort((a, b) => a.position - b.position)
+    },
+  },
+
+  async mounted() {
+    this.data = await this.getRoomRating(this.$route.params.room)
+  },
+}
+</script>

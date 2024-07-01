60fdd13142066f0bd2049e3494255119613d3bb1
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-01 18:39:47 +0500

    Update Bets.vue

--- components/Bets.vue
+++ components/Bets.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="font-oswald grid grid-cols-2 gap-4 px-4 py-1">
     <button
-      v-show="activeBet != 'up'"
+      v-show="activeSide != 'up'"
       @click="bet('up')"
       :disabled="disabled"
       class="btn btn-ghost h-16 border-2 border-lime-700 bg-lime-500 text-3xl font-bold uppercase text-white hover:border-lime-700 hover:bg-lime-500"
@@ -9,14 +9,14 @@
       Up
     </button>
     <button
-      v-show="activeBet == 'up'"
+      v-show="activeSide == 'up'"
       class="btn pointer-events-none h-16 border-2 border-lime-500 text-3xl font-bold uppercase text-lime-500"
     >
       Up
     </button>
 
     <button
-      v-show="activeBet != 'down'"
+      v-show="activeSide != 'down'"
       @click="bet('down')"
       :disabled="disabled"
       class="btn h-16 border-2 border-red-700 bg-red-500 text-3xl font-bold uppercase text-white hover:border-red-700 hover:bg-red-500"
@@ -24,7 +24,7 @@
       Down
     </button>
     <button
-      v-show="activeBet == 'down'"
+      v-show="activeSide == 'down'"
       class="btn pointer-events-none h-16 border-2 border-red-500 text-3xl font-bold uppercase text-red-500"
     >
       Down
@@ -33,56 +33,58 @@
 
   <div class="grid grid-cols-7 gap-2 p-4">
     <button
-      v-for="(bet, i) in bets"
+      v-for="(btnRate, i) in rates"
       :key="i"
-      @click="betRate = bet"
-      :class="[bet === betRate ? 'btn-outline pointer-events-none text-neutral-content' : '']"
-      :disabled="activeBet && bet != activeBetRate"
+      @click="rate = btnRate"
+      :class="[btnRate === rate ? 'btn-outline pointer-events-none text-neutral-content' : '']"
+      :disabled="activeSide && btnRate != activeRate"
       class="font-oswald btn btn-neutral border-2 text-lg"
     >
-      {{ bet }}
+      {{ btnRate }}
     </button>
   </div>
 </template>
 
 <script>
 import confetti from 'canvas-confetti'
-import { mapActions } from 'pinia'
+import { mapActions, mapState } from 'pinia'
 
 export default {
   data() {
     return {
-      activeBetRate: null,
-      activeBet: null,
-      disabled: true,
-      betRate: 5,
-      bets: [5, 10, 15, 25, 50, 100, 200],
+      activeRate: null,
+      activeSide: null,
+      rate: 5,
+      rates: [5, 10, 15, 25, 50, 100, 200],
     }
+  },
+
+  computed: {
+    ...mapState(useRoomStore, ['round_status']),
+
+    disabled() {
+      return {
+        null: true,
+        open: false,
+        running: true,
+        closed: true,
+      }[this.round_status]
+    },
   },
 
   methods: {
     ...mapActions(useWalletStore, ['placeBet']),
 
-    async bet(activeBet) {
-      this.activeBet = activeBet
-      this.activeBetRate = this.betRate
+    async bet(side) {
+      this.activeSide = side
+      this.activeRate = this.rate
       this.disabled = true
-      this.$toast.success(`Trade ${this.activeBetRate} for ${activeBet} is placed!`)
-      await this.placeBet(this.activeBet, this.activeBetRate)
-    },
-
-    manageStart({ round_status }) {
-      if (round_status == 'running') {
-        this.disabled = true
-      }
-
-      if (round_status == 'open') {
-        this.disabled = false
-      }
+      this.$toast.success(`Trade ${this.activeRate} for ${side} is placed!`)
+      await this.placeBet(this.activeSide, this.activeRate)
     },
 
     manageWinner(side) {
-      if (this.activeBet === side) {
+      if (this.activeSide === side) {
         confetti({
           particleCount: 100,
           spread: 30,
@@ -94,22 +96,20 @@ export default {
         })
 
         this.$toast('🎉 Your trade is won!')
-      } else if (this.activeBet) {
+      } else if (this.activeSide) {
         this.$toast('Your trade is failed.')
       }
 
-      this.activeBet = null
+      this.activeSide = null
       this.activeRate = null
     },
   },
 
   mounted() {
-    this.$bus.on('start', this.manageStart)
     this.$bus.on('winner', this.manageWinner)
   },
 
   unmounted() {
-    this.$bus.off('start', this.manageStart)
     this.$bus.off('winner', this.manageWinner)
   },
 }

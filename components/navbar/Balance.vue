f55dbf15fa196c96304a7005b6d55169ba951753
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-01 15:35:57 +0500

    Update balance

--- components/navbar/Balance.vue
+++ components/navbar/Balance.vue
@@ -2,18 +2,13 @@
   <button class="btn join-item btn-neutral btn-lg -mt-8 pt-4">
     <IconsWallet class="h-4 w-4 opacity-80" />
     <span class="font-oswald text-xl font-bold leading-none">
-      {{ balance || user.balance }}
+      {{ balance }}
     </span>
   </button>
 </template>
 
 <script>
-import { mapState } from 'pinia'
-
 export default {
   props: ['balance'],
-  computed: {
-    ...mapState(useUserStore, ['user']),
-  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,20 @@
   <NavbarView>
     <template #center>
       <NuxtLink to="/wallet/deposit/">
-        <NavbarBalance />
+        <NavbarBalance :balance="user.balance" />
       </NuxtLink>
     </template>
   </NavbarView>
 
   <Game />
 </template>
+
+<script>
+import { mapState } from 'pinia'
+
+export default {
+  computed: {
+    ...mapState(useUserStore, ['user']),
+  },
+}
+</script>

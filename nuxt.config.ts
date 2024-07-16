f8b7ba95ae429ca96d4cae232c24fa152b3bdafe
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-16 17:44:42 +0500

    try disable zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      viewport: 'width=device-width, initial-scale=1, user-scalable=no',
       script: [{ hid: 'stripe', src: 'https://telegram.org/js/telegram-web-app.js', defer: true }],
     },
   },

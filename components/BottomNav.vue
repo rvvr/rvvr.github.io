97c537839ce014f3552721be80c7d25a9c79ea85
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-18 18:08:29 +0500

    change main page

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="btm-nav overflow-hidden bg-base-300 drop-shadow-md">
-    <NuxtLink active-class="active text-neutral-content bg-black bg-opacity-20" to="/balance">
+    <NuxtLink active-class="active text-neutral-content bg-black bg-opacity-20" to="/">
       <button>
         <IconsWallet class="h-5 w-5" />
       </button>
       <span class="btm-nav-label text-sm">Wallet</span>
     </NuxtLink>
 
-    <NuxtLink active-class="active text-neutral-content bg-black bg-opacity-20" to="/">
+    <NuxtLink active-class="active text-neutral-content bg-black bg-opacity-20" to="/boost">
       <button>
         <IconsHome class="h-5 w-5" />
       </button>
---
+++ pages/boost.vue
@@ -0,0 +1,47 @@
+<template>
+  <NavbarView>
+    <template #center>
+      <NuxtLink to="/">
+        <NavbarBalance :balance="user.balance" />
+      </NuxtLink>
+    </template>
+  </NavbarView>
+
+  <Game />
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia'
+
+export default {
+  data() {
+    return {
+      wss: null,
+    }
+  },
+  computed: {
+    ...mapState(useUserStore, ['user']),
+  },
+  methods: {
+    ...mapActions(useRoomStore, ['openRoomSocket', 'manageSocketEvent']),
+    ...mapActions(useUserStore, ['fetchUser']),
+
+    async manageWinner() {
+      await this.fetchUser()
+    },
+  },
+  mounted() {
+    timer.start()
+    this.wss = this.openRoomSocket()
+    this.wss.onmessage = this.manageSocketEvent
+
+    this.$bus.on('winner', this.manageWinner)
+  },
+  unmounted() {
+    timer.stop()
+    this.wss.close()
+
+    this.$bus.off('winner', this.manageWinner)
+  },
+}
+</script>

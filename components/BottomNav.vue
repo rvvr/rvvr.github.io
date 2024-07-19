bda6508a9809528e7cfb45f591c183a58a3490c5
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-19 13:27:24 +0500

    Update BottomNav.vue

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="btm-nav overflow-hidden bg-base-300 drop-shadow-md">
-    <NuxtLink active-class="active text-neutral-content bg-black bg-opacity-20" to="/">
+  <div class="bg-ne btm-nav overflow-hidden bg-base-200 drop-shadow-md">
+    <NuxtLink active-class="active text-neutral-content bg-[#221551]" to="/">
       <button>
         <IconsWallet class="h-5 w-5" />
       </button>
       <span class="btm-nav-label text-sm">Wallet</span>
     </NuxtLink>
 
-    <NuxtLink active-class="active text-neutral-content bg-black bg-opacity-20" to="/boost">
+    <NuxtLink active-class="active text-neutral-content bg-[#221551]" to="/boost">
       <button>
         <IconsHome class="h-5 w-5" />
       </button>
       <span class="btm-nav-label text-sm">Boost</span>
     </NuxtLink>
 
-    <NuxtLink active-class="active text-neutral-content bg-black bg-opacity-20" to="/rooms">
+    <NuxtLink active-class="active text-neutral-content bg-[#221551]" to="/rooms">
       <button>
         <IconsFree class="h-6 w-6" />
       </button>

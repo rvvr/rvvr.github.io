e055c72c6f950f10405c2d6dcb1203374809f02b
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-24 10:44:45 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@pinia/nuxt', '@sidebase/nuxt-auth'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@pinia/nuxt'],
 
   plugins: ['~/plugins/init.client.js', '~/plugins/events.client.js'],
 

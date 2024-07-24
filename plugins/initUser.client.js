473e8cd57aa3a85e066bc569e1a98628a6d5d83d
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-24 17:03:25 +0500

    Update initUser.client.js

--- plugins/initUser.client.js
+++ plugins/initUser.client.js
@@ -8,14 +8,17 @@ export default defineNuxtPlugin(async () => {
 
   userStore.getUserFromApp()
   if (isDev()) userStore.appUser.id = 1524648
+  console.log('userStore.appUser.id', userStore.appUser.id)
   if (!userStore.appUser.id) showError(e)
 
   await userStore.getToken()
+  console.log('userStore.token', userStore.token)
   if (isDev()) userStore.token = 'test'
   if (!userStore.token) showError(e)
 
   await userStore.fetchUser()
   if (!userStore.user.user_id) await userStore.regUser()
+  console.log('userStore.user.user_id:', userStore.user.user_id)
   if (!userStore.user.user_id) showError(e)
 
   await userStore.setAvatar()

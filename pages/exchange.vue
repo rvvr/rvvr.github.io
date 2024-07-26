1cf471ed815acefbe9ea83c2596d8f7dec837d0b
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-26 15:45:44 +0500

    Exchange

--- pages/exchange.vue
+++ pages/exchange.vue
@@ -4,4 +4,20 @@
       <div class="font-bold text-neutral-content">Exchange</div>
     </template>
   </NavbarView>
+
+  <div class="flex-1 p-4">
+    <div class="font-oswald card-title mt-8">Trading Made Easy</div>
+    <p class="mt-4 text-left opacity-50">
+      Cryptobull offers a diverse selection of trading pairs, including popular cryptocurrencies like Bitcoin,
+      Ethereum, and Litecoin, as well as newer, emerging tokens. The exchange also supports a range of fiat
+      currencies, making it easy for users to deposit and withdraw funds.
+    </p>
+
+    <div class="text-center">
+      <div class="mb-4 mt-14">
+        <img class="inline w-60 contrast-200 grayscale invert filter" alt="" src="/exchange.png" />
+      </div>
+      <button class="btn btn-accent">Exchange coins on Cryptobull.io</button>
+    </div>
+  </div>
 </template>

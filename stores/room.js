a13af17c336998be1431c8a02e0ae23585304dd6
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-26 15:08:48 +0500

    Update room.js

--- stores/room.js
+++ stores/room.js
@@ -60,7 +60,10 @@ export const useRoomStore = defineStore('room', {
     },
 
     async joinRoom(room_id) {
-      return await api.post('/room/join', { room_id })
+      return await api.post('/room/join', {
+        user_id: this.user_id,
+        room_id,
+      })
     },
 
     openRoomSocket(room_id) {

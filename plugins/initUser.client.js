0f6830168ccace5745907f4b1e8ff248663368b0
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-07-29 13:37:44 +0500

    Dev data

--- plugins/initUser.client.js
+++ plugins/initUser.client.js
@@ -2,11 +2,9 @@ export default defineNuxtPlugin(async () => {
   const userStore = useUserStore()
 
   userStore.getUserFromApp()
-  if (isDev()) userStore.appUser.id = 1524648
   if (!userStore.appUser.id) return
 
   await userStore.getToken()
-  if (isDev()) userStore.token = '132ba3cb70a061eb43b727028ccda41b'
   if (!userStore.token) return
 
   await userStore.fetchUser()
--- stores/user.js
+++ stores/user.js
@@ -18,7 +18,15 @@ export const useUserStore = defineStore('user', {
 
   actions: {
     getUserFromApp() {
-      this.appUser = { ...window.Telegram?.WebApp.initDataUnsafe.user }
+      if (isDev()) {
+        this.appUser = {
+          id: 1524648,
+          username: 'rvvvr',
+          first_name: 'RVR',
+        }
+      } else {
+        this.appUser = { ...window.Telegram?.WebApp.initDataUnsafe.user }
+      }
     },
 
     async fetchUser() {
@@ -34,7 +42,14 @@ export const useUserStore = defineStore('user', {
     },
 
     async getToken() {
-      const { token } = await api.post('/auth/signup', { data: window.Telegram?.WebApp.initData })
+      let data
+      if (isDev()) {
+        data =
+          'query_id=AAGoQxcAAAAAAKhDFwB0oiaK&user=%7B%22id%22%3A1524648%2C%22first_name%22%3A%22%F0%9D%99%8D%F0%9D%99%91%F0%9D%99%8D%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22rvvvr%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1721635535&hash=be56525b0685c9683a959c42d1958526f38ff89aa90f1720eda80df0cdb1c7e5'
+      } else {
+        data = window.Telegram?.WebApp.initData
+      }
+      const { token } = await api.post('/auth/signup', { data })
       this.token = token
     },
 

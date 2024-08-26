91130a09cae97d6abec5a31702ba9efb0114aac4
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-08-26 17:02:37 +0500

    Update withdraw.vue

--- pages/rooms/withdraw.vue
+++ pages/rooms/withdraw.vue
@@ -1,14 +1,14 @@
 <template>
   <NavbarView>
     <template #center>
-      <div class="font-bold text-neutral-content">Withdraw</div>
+      <div class="font-bold text-neutral-content">Rewards</div>
     </template>
   </NavbarView>
 
   <div class="flex-1 p-4">
     <div class="text-center">
+      <span>Total</span>
       <div class="mb-4 flex items-baseline justify-center">
-        <span><IconsWallet class="mr-2 h-3 w-3 opacity-50" /></span>
         <span class="text-2xl font-bold">9999</span>
         <span class="ml-1 text-xs opacity-50">TON</span>
       </div>
@@ -16,44 +16,13 @@
       <button class="mb-2 h-10" id="connect"></button>
     </div>
 
-    <div v-if="wallet.address">
-      <p class="py-2 opacity-50">
-        Direct withdrawal trough blockchain can take up to 10 minutes. Please be patient!
-      </p>
+    <p v-if="!wallet.address" class="py-2 opacity-50">
+      Connect your TON wallet to proceed with the withdrawal.
+    </p>
 
-      <label class="form-control mt-2 w-full">
-        <div class="label">
-          <span class="label-text-alt">Withdraw Address</span>
-        </div>
-        <input
-          :value="wallet.address"
-          class="input input-bordered w-full"
-          placeholder="Enter your withdraw Address"
-          type="text"
-        />
-      </label>
-
-      <label class="form-control mt-2 w-full">
-        <div class="label">
-          <span class="label-text-alt">Withdraw Amount</span>
-          <span class="label-text-alt">Minimum withdrawal: 1</span>
-        </div>
-        <div class="join border">
-          <input class="input join-item input-bordered w-full" type="text" value="0.00" />
-          <button class="btn btn-square join-item">25%</button>
-          <button class="btn btn-square join-item">50%</button>
-          <button class="btn btn-square join-item">100%</button>
-        </div>
-      </label>
-
-      <div class="btn btn-neutral mt-6 w-full">Withdraw</div>
-    </div>
-
-    <p v-else class="py-2 opacity-50">Connect your TON wallet to proceed with the withdrawal.</p>
-
-    <div class="mb-2 mt-8 flex items-baseline text-lg font-bold">
+    <div class="mb-2 mt-4 flex items-baseline text-lg font-bold">
       <IconsTrophy class="mr-2 h-4 w-4 text-neutral-content" />
-      Winning history
+      Winnings
     </div>
 
     <div class="-ml-2 -mr-2 overflow-x-auto">
@@ -62,25 +31,30 @@
           <tr>
             <th>Date</th>
             <th class="text-center">Place</th>
-            <th class="text-right">Amount</th>
+            <th class="text-center">Amount</th>
+            <th class="text-center">Reward</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>13.02.2000</td>
             <td class="text-center">1</td>
-            <td class="text-right">
+            <td class="text-center">
               15
               <span class="text-xs opacity-50">TON</span>
             </td>
+            <td class="text-center">
+              <button class="btn btn-neutral btn-xs">Recieve</button>
+            </td>
           </tr>
           <tr>
-            <td>12.02.2000</td>
-            <td class="text-center">2</td>
-            <td class="text-right">
-              10
+            <td>13.02.2000</td>
+            <td class="text-center">1</td>
+            <td class="text-center">
+              15
               <span class="text-xs opacity-50">TON</span>
             </td>
+            <td class="text-center"><div class="leading-6 text-white text-opacity-50">Sended</div></td>
           </tr>
         </tbody>
       </table>

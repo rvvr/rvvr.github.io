53d588cacb66ec7785b67d3c11726946d6d87d1a
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-08-29 13:09:42 +0500

    Update usdFormat.js

--- composables/usdFormat.js
+++ composables/usdFormat.js
@@ -1,9 +1,6 @@
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
-
-  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 })
 
 export const usdFormat = (num) => formatter.format(num)

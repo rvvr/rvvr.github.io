0c3fb40cc1796a51142d7716f03ef86a7d9cf54e
Vladimir Romanov <17041300+rvvr@users.noreply.github.com>
2024-10-04 12:53:46 +0500

    Update Countdown.vue

--- components/Countdown.vue
+++ components/Countdown.vue
@@ -27,7 +27,7 @@ export default {
 
     const date = dayjs.unix(this.date)
     if (Date.now() > date) return
-    this.timer = countdown.default(date, (ts) => {
+    this.timer = countdown(date, (ts) => {
       console.log('🚀 ~ this.timer=countdown.default ~ countdown:', countdown)
       if (ts.end < ts.start) this.output = ts.minutes + ':' + `${ts.seconds}`.padStart(2, '0')
       else this.stop()
